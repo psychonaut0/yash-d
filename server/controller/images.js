c472143cb7563e775114fcb8ba935149ff752c7d
psychonaut <checco0002@gmail.com>
2022-11-15 19:59:41 +0100

    some doc

--- server/controller/images.js
+++ server/controller/images.js
@@ -1,91 +1,132 @@
-const asyncHandler = require('express-async-handler')
-const Image = require('../models/image')
-const moment = require('moment')
+const asyncHandler = require("express-async-handler");
+const Image = require("../models/image");
+const moment = require("moment");
 
-const fs = require('fs')
+const fs = require("fs");
 
 //init uploader
-const multer = require("multer")
+const multer = require("multer");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/images')
+    cb(null, "public/images");
   },
   filename: (req, file, cb) => {
-    const lastIndex = file.originalname.lastIndexOf('.')
-    cb(null, `${file.originalname.substring(0, lastIndex)}__${moment().format()}${file.originalname.substring(lastIndex)}`)
-  }
-})
-const upload = multer({storage: storage})
+    const lastIndex = file.originalname.lastIndexOf(".");
+    cb(
+      null,
+      `${file.originalname.substring(
+        0,
+        lastIndex
+      )}__${moment().format()}${file.originalname.substring(lastIndex)}`
+    );
+  },
+});
+const upload = multer({ storage: storage });
 
-
-
-const getImages = asyncHandler( async (req, res) => {
+/**
+ * - Method GET
+ * - Retrieve all images from Image collection.
+ * @function getImages
+ * @description Method GET
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ */
+const getImages = asyncHandler(async (req, res) => {
   const images = await Image.find();
 
-  res.status(200).json(images)
-})
+  res.status(200).json(images);
+});
 
+/**
+ * - Method GET
+ * - Retrieve an image from Image collection
+ * @function getImage
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ */
 const getImage = asyncHandler(async (req, res) => {
-
-  if(!req.body.id){
-    res.status(400)
-    throw new Error("id is required to get an image. Please provide an id")
+  if (!req.body.id) {
+    res.status(400);
+    throw new Error("id is required to get an image. Please provide an id");
   }
 
-  const image = await Image.findById(req.body.id)
+  const image = await Image.findById(req.body.id);
 
+  res.status(200).json(image);
+});
 
-  res.status(200).json(image)
-})
+/**
+ * - Method POST
+ * - Add an image to Image collection
+ * @function getImage
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ */
+const setImage = [
+  upload.single("image"),
+  asyncHandler(async (req, res) => {
+    const image = await Image.create({
+      sourceUrl: `${req.protocol}://${req.get("host")}/images/${
+        req.file.filename
+      }`,
+      relativeUrl: `/images/${req.file.filename}`,
+      filename: req.file.filename,
+    });
 
-const setImage = [upload.single("image"), asyncHandler(async (req, res) => {
+    res.status(200).json(image);
+  }),
+];
 
-  const image = await Image.create({
-    sourceUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-    relativeUrl: `/images/${req.file.filename}`,
-    filename: req.file.filename
-  })
-
-  res.status(200).json(image)
-})]
-
-const updateImage = asyncHandler(async (req,res) => {
-
-
-  if(!req.body.filename){
-    res.status(400)
-    throw new Error('Filename is required. No filename provided')
+/**
+ * - Method PUT
+ * - Update an image to Image collection
+ * @function getImage
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ */
+const updateImage = asyncHandler(async (req, res) => {
+  if (!req.body.filename) {
+    res.status(400);
+    throw new Error("Filename is required. No filename provided");
   }
 
-  let filename = req.body.filename
+  let filename = req.body.filename;
 
+  const lastIndex = filename.lastIndexOf(".");
 
-  const lastIndex = filename.lastIndexOf('.')
+  const image = await Image.findById(req.params.id);
 
-
-  const image = await Image.findById(req.params.id)
-
-  if(lastIndex === -1){
-    filename = `${filename}${image.filename.substring(image.filename.lastIndexOf('.'))}`;
+  if (lastIndex === -1) {
+    filename = `${filename}${image.filename.substring(
+      image.filename.lastIndexOf(".")
+    )}`;
   }
 
-  fs.rename(`public/images/${image.filename}`, `public/images/${filename}`, () => {
-    console.log("file renamed!")
-  })
+  fs.rename(
+    `public/images/${image.filename}`,
+    `public/images/${filename}`,
+    () => {
+      console.log("file renamed!");
+    }
+  );
 
+  image.filename = filename;
+  image.sourceUrl = `${req.protocol}://${req.get("host")}/images/${filename}`;
+  image.relativeUrl = `/images/${filename}`;
 
-  image.filename = filename
-  image.sourceUrl = `${req.protocol}://${req.get('host')}/images/${filename}`
-  image.relativeUrl = `/images/${filename}`  
+  await image.save();
 
-  await image.save()
+  res.status(200).json(image);
+});
 
-
-  res.status(200).json(image)
-})
-
-const deleteImage = asyncHandler(async (req,res) => {
-
+/**
+ * - Method DELETE
+ * - Delete an image from Image collection
+ * @function getImage
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ */
+const deleteImage = asyncHandler(async (req, res) => {
   const image = await Image.findById(req.params.id);
 
   if (!image) {
@@ -95,15 +136,13 @@ const deleteImage = asyncHandler(async (req,res) => {
 
   const deletedTile = await Image.findByIdAndDelete(req.params.id, req.body);
 
-  res.status(200).json(deletedTile)
-})
-
+  res.status(200).json(deletedTile);
+});
 
 module.exports = {
   getImage,
   getImages,
   setImage,
   updateImage,
-  deleteImage
-}
-
+  deleteImage,
+};

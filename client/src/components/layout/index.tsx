206b600116058605db0b1179c69cf980e0bea716
psychonaut <checco0002@gmail.com>
2022-12-05 23:24:11 +0100

    fix sidebar

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -31,11 +31,16 @@ export default function Layout({ children }: Props) {
       <div className="w-full h-full flex flex-col items-center min-h-screen bg-dark text-light">
         {
           !user?.isLoading &&
-            <Header user={user.data} />
+          <Header user={user.data} />
         }
         <div className="flex w-full h-full max-w-[1800px] relative z-20">
           <Sidebar groups={groups.data} />
           <div className="w-full min-h-[700px] rounded-3xl bg-dark-800 border-dark-600 border-2 p-4 md:p-20">
+            <div className="fixed   -left-48 w-screen h-screen bg-transparent pointer-events-none font-bold text-[600px] flex text-primary opacity-5 ">
+              <span className="whitespace-nowrap">
+                ヤシダイ
+              </span>
+            </div>
             <Outlet />
           </div>
           <div className="md:w-10" />
